6c551900e158a1a7fa8e39cb7149580e4a13983c
SalvadorVillagomezMartinez <sa.villagomez1006@gmail.com>
2022-08-09 11:15:33 -0500

    Commit 2

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProductosController extends Controller
 {
-    public function index(){
-        
+    public function getProductos(){
+        return view('productos.list');
     }
 }
